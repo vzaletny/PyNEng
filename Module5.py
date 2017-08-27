8e5b42982ae93305268e66a220ecbb6830c296e7
vzaletny <vzaletny@gmail.com>
2017-08-27 21:04:28 +0700

    Change some code

--- Module5.py
+++ Module5.py
@@ -17,6 +17,11 @@ elif len(tuple(i for i in ip if i == 255)) == 4:
 else:
     print('unused')
 
+'''
+print(all(i == 0 for i in ip))
+print(all(i == 255 for i in ip))
+'''
+
 mac = ['aabb:cc80:7000', 'aabb:dd80:7340', 'aabb:ee80:7000', 'aabb:ff80:7000']
 mac_cisco = [i.replace(':', '.') for i in mac]
 print(mac_cisco)
