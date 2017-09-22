2c82041626804cc81444b7a22df06a3e3a3a9d1b
vzaletny <vzaletny@gmail.com>
2017-09-22 19:47:55 +0700

    Add solution to 7.3a

--- 7/7.3a.py
+++ 7/7.3a.py
@@ -4,6 +4,31 @@ from pprint import pprint
 
 
 def get_int_vlan_map(conf_file):
+    """
+    Сделать копию скрипта задания 7.3.
+
+    Дополнить скрипт:
+        - добавить поддержку конфигурации, когда настройка access-порта выглядит так:
+                interface FastEthernet0/20
+                    switchport mode access
+                    duplex auto
+          То есть, порт находится в VLAN 1
+
+    В таком случае, в словарь портов должна добавляться информация, что порт в VLAN 1
+          Пример словаря: {'FastEthernet0/12':10,
+                           'FastEthernet0/14':11,
+                           'FastEthernet0/20':1 }
+
+    Функция ожидает в качестве аргумента имя конфигурационного файла.
+
+    Проверить работу функции на примере файла config_sw2.txt
+
+
+    Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+    :param conf_file:
+    :return:
+    """
     try:
         with open(conf_file, 'r', encoding='utf-8') as f:
             last_interface = ''
@@ -21,13 +46,16 @@ def get_int_vlan_map(conf_file):
                 elif 'access vlan' in line:
                     if intf == last_interface:
                         access_dict[intf] = int(line.split()[-1])
+                elif 'mode access' in line:
+                    if intf == last_interface and not access_dict.get(intf):
+                        access_dict[intf] = 1
             return trunk_dict, access_dict
     except FileNotFoundError:
         print('File not found')
         return
 
 
-ifile = './PyNEng/7/config_sw1.txt'
+ifile = './PyNEng/7/config_sw2.txt'
 dict1, dict2 = get_int_vlan_map(ifile)
 pprint(dict1)
 pprint(dict2)
